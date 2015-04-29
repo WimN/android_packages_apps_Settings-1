57d7c5fb4f97aa2ff4fbbf2d56065159912b47a5
John Brewer <gzroms@hotmail.com>
2015-04-29 18:51:57 -0400

    Few more color edits

--- src/com/android/settings/cyanogenmod/qs/DraggableGridView.java
+++ src/com/android/settings/cyanogenmod/qs/DraggableGridView.java
@@ -132,14 +132,14 @@ public class DraggableGridView extends ViewGroup implements
     public void addView(View child, int index) {
         super.addView(child, index);
         mNewPositions.add(-1);
-        child.setBackgroundColor(0xff242424);
+        child.setBackgroundColor(0xff424242);
     }
 
     @Override
     public void addView(View child) {
         super.addView(child);
         mNewPositions.add(-1);
-        child.setBackgroundColor(0xff242424);
+        child.setBackgroundColor(0xff424242);
     };
 
     @Override
